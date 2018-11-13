1c716c7b8ad301ac95d79ff662bd9de9c4c35d4f
Prabhsimran Singh <pskrunner14@gmail.com>
2018-11-13 10:59:43 +0530

    minor changes to test

--- tests/test_singly_linked_list.cpp
+++ tests/test_singly_linked_list.cpp
@@ -5,11 +5,21 @@ using namespace std;
 #include <cpp_algs.hpp>
 
 TEST_CASE("Singly Linked List test", "[DS]") {
-    ds::SinglyLinkedList<int> sing = ds::SinglyLinkedList<int>();
-    REQUIRE(sing.getSize() == 0);
-    sing.insertNode(101);
-    sing.insertNode(432);
-    REQUIRE(sing.getSize() == 2);
-    sing.deleteNode(432);
-    REQUIRE(sing.getSize() == 1);
+    ds::SinglyLinkedList<int> *sing = new ds::SinglyLinkedList<int>();
+    REQUIRE(sing->getSize() == 0);
+
+    sing->insertNode(101);
+    sing->insertNode(432);
+    REQUIRE(sing->getSize() == 2);
+
+    sing->deleteNode(432);
+    REQUIRE(sing->getSize() == 1);
+
+    int *arr = new int[5]{43, 44, 45, 46, 47};
+    sing->insertArray(arr, 5);
+    REQUIRE(sing->getSize() == 6);
+
+    // cleanup
+    delete[] arr;
+    delete sing;
 }
--- tests/verify.cpp
+++ tests/verify.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 using namespace std;
 
@@ -16,6 +17,11 @@ int main() {
     s.insertNode("world");
     s.insertNode("!");
     s.print();
+
+    int *arr = new int[5]{43, 44, 45, 46, 47};
+    sing.insertArray(arr, 5);
+
+    sing.print();
 
     cout << "Verified installation of library!" << '\n';
     return 0;
