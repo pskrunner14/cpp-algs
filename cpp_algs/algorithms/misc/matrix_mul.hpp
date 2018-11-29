df83ce7d0826f5ba4a00682bbc8131483794f921
Prabhsimran Singh <pskrunner14@gmail.com>
2018-11-29 16:04:27 +0530

    ref matmul

--- cpp_algs/algorithms/misc/matrix_mul.hpp
+++ cpp_algs/algorithms/misc/matrix_mul.hpp
@@ -29,23 +29,22 @@ std::vector<std::vector<T>> matmul(const std::vector<std::vector<T>> &, const st
 */
 template <typename T>
 std::vector<std::vector<T>> matmul(const std::vector<std::vector<T>> &a, const std::vector<std::vector<T>> &b) {
-    std::vector<std::vector<T>> out;
     int m = a.size();
     int n = a[0].size();
     int k = b[0].size();
 
     BOOST_ASSERT_MSG(n == b.size(), "No. of columns in first matrix don't match no. of rows in second.");
 
+    std::vector<std::vector<T>> out(m, std::vector<T>(k));
+
     for (int i = 0; i < m; i++) {
-        std::vector<T> current;
         for (int j = 0; j < k; j++) {
             T sum = 0;
             for (int c = 0; c < n; c++) {
                 sum += a[i][c] * b[c][j];
             }
-            current.push_back(sum);
+            out[i][j] = sum;
         }
-        out.push_back(current);
     }
     return out;
 }
